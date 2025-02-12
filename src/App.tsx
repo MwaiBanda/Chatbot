4d1f23488059c9a92c80bba2fb9009936d691942
Mwai Banda <bandamwai@gmail.com>
2025-02-11 19:32:28 -0600

    feat: add utility functions and UI components with Tailwind CSS integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
-import { useState } from 'react'
 import './App.css'
 
-function App() {
 
+type Option = {
+  id: number
+  name: string
+  type: OptionType
+}
+
+enum OptionType {
+  Departments = 'Departments',
+  Courses = 'Courses',
+  Locations = 'Locations',
+  Admissions = 'Admissions',
+  Contact = 'Contact',
+  AboutUs = 'About Us',
+}
+
+function App() {
+  const options: Option[] = [
+    { id: 1, name: 'Departments', type: OptionType.Departments },
+    { id: 2, name: 'Courses', type: OptionType.Courses },
+    { id: 3, name: 'Locations', type: OptionType.Locations },
+    { id: 4, name: 'Admssions', type: OptionType.Admissions },
+    { id: 5, name: 'Contact', type: OptionType.Contact },
+    { id: 6, name: 'About Us',  type: OptionType.AboutUs },
+  ]
   return (
     <>
-      <div>
-     
+      <div className='card'>
+       <div className="header">
+        <p className="title">Ivy Tech Community College</p>
+       </div>
+       <hr className="solid"/>
+       <div className="content">
+        <div className="content-container">
+        <div className="response">
+            Hello 👋🏾
+          </div>
+        </div>
+      
+       </div>
+       <div className="options">
+        {options.map((option) => {
+          return <div className="tab">
+            <div className="tab-name">
+              {option.name}
+            </div>
+          </div>
+        })}
+       </div>
       </div>
     </>
   )
